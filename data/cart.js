3ed715388e181754de19533ea3453a761b4e1f15
adityakhadsecode <adityakhadse.dev@gmail.com>
2025-09-29 01:11:21 +0530

    Moved orderSummary to its own file

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,5 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
-if (!cart) {
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+if (cart.length === 0) {
   cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
